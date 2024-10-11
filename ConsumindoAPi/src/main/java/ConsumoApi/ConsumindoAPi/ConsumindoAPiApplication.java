8989754a2efd0729ec748a581d7090a7b78a3fa8
Yuri <yurilinsantoniazzi2004@outlook.com>
2024-10-11 08:35:06 -0300

    Add files via upload

---
+++ ConsumindoAPi/src/main/java/ConsumoApi/ConsumindoAPi/ConsumindoAPiApplication.java
@@ -0,0 +1,13 @@
+package ConsumoApi.ConsumindoAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumindoAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumindoAPiApplication.class, args);
+	}
+
+}
---
+++ ConsumindoAPi/src/test/java/ConsumoApi/ConsumindoAPi/ConsumindoAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package ConsumoApi.ConsumindoAPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumindoAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
